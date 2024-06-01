c672dd88b1feb19633c24ae89e1e0b0be94eb0dd
Vaishali-Sahu-04 <vaishusahuone@gmail.com>
2024-06-01 19:41:23 +0530

    modified url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URI = 'https://file-sharing-ohyp.onrender.com/';
+const API_URI = 'https://file-sharing-ohyp.onrender.com';
 
 export const uploadFile = async (data) => {
     try {
